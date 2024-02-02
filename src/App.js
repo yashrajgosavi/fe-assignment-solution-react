5425a34eb0f9113ca3ff10abb0ad08b39b871a8b
NuraYokai <yashrajgosavi2580@gmail.com>
2024-02-02 17:34:50 +0530

    feat: add input for search added input style

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import "./App.css";
 
 function App() {
   const [sortType, setSortType] = useState("year");
+  const [searchItem, setSearchItem] = useState("");
   return (
-    <select onChange={(e) => setSortType(e.target.value)}>
-      <option value="year">YEAR</option>
-      <option value="episode">EPISODE</option>
-    </select>
+    <div>
+      <select onChange={(e) => setSortType(e.target.value)}>
+        <option value="year">YEAR</option>
+        <option value="episode">EPISODE</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Search by title"
+        value={searchItem}
+        onChange={(e) => setSearchItem(e.target.value)}
+      />
+    </div>
   );
 }
 
